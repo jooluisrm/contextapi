f352bab8e5524dd06b1fdf0b1fc3fad075eaab6a
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-08 10:06:48 -0300

    Criando Context com Reducers 2

--- src/contexts/Context.tsx
+++ src/contexts/Context.tsx
@@ -1,2 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import { userInitialState, userReducer, UserType } from "../reducers/userReducer";
+import { reducerActionType } from "../Types/reducerActionType";
 
 
+
+type initialStateType = {
+    user: UserType;
+}
+
+type ContextType = {
+    state: initialStateType;
+    dispatch: React.Dispatch<any>;
+}
+
+const initialState = {
+    user: userInitialState
+}
+
+export const Context = createContext<ContextType>({
+    state: initialState,
+    dispatch: () => null
+});
+
+const mainReducer = (state: initialStateType, action: reducerActionType) => ({
+    user: userReducer(state.user, action)
+});
