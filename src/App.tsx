360ef258443dc65ad254b4f1afdb7543929f8bff
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-08 09:18:57 -0300

    ContextAPI na prática

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SignUp } from './pages/SignUp';
+import { ShowData } from './pages/ShowData';
+const App = () => {
+    return (
+        <BrowserRouter>
+            <h1>Titulo da página</h1>
+            <hr />
+            <Routes>
+                <Route path="/" element={<SignUp />}/>
+                <Route path="/exibir" element={<ShowData />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
