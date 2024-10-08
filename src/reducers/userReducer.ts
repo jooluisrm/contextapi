18d1661550f34b7f1d649c139e2336d9d4ef066d
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-08 09:55:57 -0300

    Criando Context com Reducers 1

---
+++ src/reducers/userReducer.ts
@@ -0,0 +1,25 @@
+import { reducerActionType } from "../Types/reducerActionType";
+
+export type UserType = {
+    name: string;
+    age: number;
+}
+
+export const userInitialState: UserType = {
+    name: 'João Luís',
+    age: 19
+}
+
+export const userReducer = (state: UserType, action: reducerActionType) => {
+
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {...state, name: action.payload.name};
+        break;
+        case 'CHANGE_AGE':
+            return {...state, age: action.payload.age};
+        break;
+    }
+
+    return state;
+}
