f420fa10994ea43ec2d82d321d609482e7e246ef
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-08 09:45:53 -0300

    Acessando valores do context

--- src/pages/ShowData.tsx
+++ src/pages/ShowData.tsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../contexts/Context";
 
 export const ShowData = () => {
+
+    const { age, name } = useContext(Context);
+
     return (
         <div>
-            Tela ShowData
+            Tela ShowData de {name}!
             <br />
             <Link to={'/'}>Voltar para SignUp</Link>
         </div>
